b14d7c554ceb44f0daea2fe39ca86cae9a119fd9
isu502884 <yagi16k@gmail.com>
2026-04-12 10:57:39 +0900

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio-sepia-eta-83.vercel.app/',
   integrations: [tailwind()],
 });
